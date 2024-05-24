16690a1640b0128380cf06f10812e41b8b9d20c0
Jordan Kay <jordan@fleuronic.com>
2024-05-23 20:18:51 -0400

    Fix formatting

--- WorkflowUI/Sources/Hosting/WorkflowHostingController.swift
+++ WorkflowUI/Sources/Hosting/WorkflowHostingController.swift
@@ -77,6 +77,7 @@ public final class WorkflowHostingController<ScreenType, Output>: UIViewControll
         workflowHost.update(workflow: workflow.asAnyWorkflow())
     }
 
+    @available(*, unavailable)
     public required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
