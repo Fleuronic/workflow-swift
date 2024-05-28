9466fe8eb06ccf2b0548ad7325469918f4be87b4
Jordan Kay <jordan@fleuronic.com>
2024-05-28 15:32:13 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,85 +1,76 @@
 // swift-tools-version:5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
-    name: "Workflow",
-    platforms: [
+	name: "Workflow",
+	platforms: [
 		.iOS(.v13),
 		.macOS(.v10_15),
 		.tvOS(.v13),
 		.watchOS(.v6)
-    ],
-    products: [
-        .library(
-            name: "Workflow",
-            targets: ["Workflow"]
-        ),
-        .library(
-            name: "WorkflowTesting",
-            targets: ["WorkflowTesting"]
-        ),
+	],
+	products: [
 		.library(
-            name: "WorkflowUI",
-            targets: ["WorkflowUI"]
-        ),
+			name: "Workflow",
+			targets: ["Workflow"]
+		),
 		.library(
-            name: "WorkflowReactiveSwift",
-            targets: ["WorkflowReactiveSwift"]
-        ),
+			name: "WorkflowUI",
+			targets: ["WorkflowUI"]
+		),
 		.library(
-            name: "ViewEnvironment",
-            targets: ["ViewEnvironment"]
-        ),
-    ],
-    dependencies: [.package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", from: "7.1.1")],
-    targets: [
-        .target(
-            name: "Workflow",
-            dependencies: ["ReactiveSwift"],
-            path: "Workflow/Sources"
-        ),
-        .testTarget(
-            name: "WorkflowTests",
-            dependencies: ["Workflow"],
-            path: "Workflow/Tests"
-        ),
-        .target(
-            name: "WorkflowTesting",
-            dependencies: ["Workflow"],
-            path: "WorkflowTesting/Sources"
-        ),
-        .testTarget(
-            name: "WorkflowTestingTests",
-            dependencies: ["WorkflowTesting"],
-            path: "WorkflowTesting/Tests"
-        ),
-        .target(
-            name: "WorkflowUI",
-            dependencies: ["Workflow", "ViewEnvironment"],
-            path: "WorkflowUI/Sources"
-        ),
-        .testTarget(
-            name: "WorkflowUITests",
-            dependencies: ["WorkflowUI", "WorkflowReactiveSwift"],
-            path: "WorkflowUI/Tests"
-        ),
-
-        // MARK: WorkflowReactiveSwift
-
-        .target(
-            name: "WorkflowReactiveSwift",
-            dependencies: ["ReactiveSwift", "Workflow"],
-            path: "WorkflowReactiveSwift/Sources"
-        ),
-
-        // MARK: ViewEnvironment
-
-        .target(
-            name: "ViewEnvironment",
-            path: "ViewEnvironment/Sources"
-        ),
-    ],
-    swiftLanguageVersions: [.v5]
+			name: "WorkflowReactiveSwift",
+			targets: ["WorkflowReactiveSwift"]
+		),
+		.library(
+			name: "WorkflowTesting",
+			targets: ["WorkflowTesting"]
+		),
+		.library(
+			name: "ViewEnvironment",
+			targets: ["ViewEnvironment"]
+		),
+	],
+	dependencies: [.package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", from: "7.1.1")],
+	targets: [
+		.target(
+			name: "Workflow",
+			dependencies: ["ReactiveSwift"],
+			path: "Workflow/Sources"
+		),
+		.testTarget(
+			name: "WorkflowTests",
+			dependencies: ["Workflow"],
+			path: "Workflow/Tests"
+		),
+		.target(
+			name: "WorkflowUI",
+			dependencies: ["Workflow", "ViewEnvironment"],
+			path: "WorkflowUI/Sources"
+		),
+		.testTarget(
+			name: "WorkflowUITests",
+			dependencies: ["WorkflowUI", "WorkflowReactiveSwift"],
+			path: "WorkflowUI/Tests"
+		),
+		.target(
+			name: "WorkflowReactiveSwift",
+			dependencies: ["ReactiveSwift", "Workflow"],
+			path: "WorkflowReactiveSwift/Sources"
+		),
+		.target(
+			name: "WorkflowTesting",
+			dependencies: ["Workflow"],
+			path: "WorkflowTesting/Sources"
+		),
+		.testTarget(
+			name: "WorkflowTestingTests",
+			dependencies: ["WorkflowTesting"],
+			path: "WorkflowTesting/Tests"
+		),
+		.target(
+			name: "ViewEnvironment",
+			path: "ViewEnvironment/Sources"
+		)
+	]
 )
